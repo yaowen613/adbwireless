3d5bb42f1192614bae520c6adadac63d0172a39d
茗香墨语 <yaowen424@126.com>
2015-10-08 17:10:36 +0800

    修改了样式

--- app/build/generated/source/r/debug/siir/es/adbWireless/R.java
+++ app/build/generated/source/r/debug/siir/es/adbWireless/R.java
@@ -21,15 +21,15 @@ public final class R {
     }
     public static final class id {
         public static final int FrameLayout01=0x7f070000;
-        public static final int LinearLayout01=0x7f070004;
-        public static final int iv_button=0x7f070005;
+        public static final int LinearLayout01=0x7f070007;
+        public static final int iv_button=0x7f070008;
         public static final int menu_about=0x7f07000a;
         public static final int menu_exit=0x7f07000b;
         public static final int menu_prefs=0x7f070009;
         public static final int scrollView1=0x7f070002;
-        public static final int tv_footer_1=0x7f070006;
-        public static final int tv_footer_2=0x7f070007;
-        public static final int tv_footer_3=0x7f070008;
+        public static final int tv_footer_1=0x7f070004;
+        public static final int tv_footer_2=0x7f070005;
+        public static final int tv_footer_3=0x7f070006;
         public static final int tv_top_text=0x7f070003;
         public static final int widgetButton=0x7f070001;
     }
